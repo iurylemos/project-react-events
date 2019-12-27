53b139d6b2c1e42e245b102f15f1e3246ed9d063
Iury Lemos <iurylemos10@gmail.com>
2019-12-27 14:08:01 -0300

    Autenticação com o firebase com êxito

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css'
+import firebase from '../../config/firebase'
+
+//authentication
+import 'firebase/auth'
 
 function Login() {
+
+    //Usando o useState
+    //Ele guarda o estado do componente e os dados
+    //Conhecido como hooks do react também
+    //Gravar estados de componente funcionais, ou seja componentes que chamam funções
+    //Eles vão amazenar os conteudos dos campos 
+
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+
+    function logar() {
+        // alert('VAMOS LOGAR')
+        firebase.auth().signInWithEmailAndPassword(email, senha).then((resultado) => {
+            alert('USUÁRIO LOGADO')
+        }).catch(error => {
+            console.log(error)
+            alert(error)
+        })
+    }
+
+
     return (
         <div className="login-content d-flex align-items-center">
             <form className="form-signin mx-auto">
                 <img className="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" />
                 <h1 className="h3 mb-3 font-weight-normal text-white font-weight-bold text-center">LOGIN</h1>
                 <label className="sr-only">Email address</label>
-                <input type="email" id="inputEmail" className="form-control my-2" placeholder="Email" />
+                <input onChange={(e) => setEmail(e.target.value)} type="email" id="inputEmail" className="form-control my-2" placeholder="Email" />
                 <label className="sr-only">Password</label>
-                <input type="password" id="inputPassword" className="form-control my-2" placeholder="Senha" />
-                <button className="btn btn-lg btn-block btn-login" type="submit">Sign in</button>
+                <input onChange={(e) => setSenha(e.target.value)} type="password" id="inputPassword" className="form-control my-2" placeholder="Senha" />
+                <button className="btn btn-lg btn-block btn-login" type="button" onClick={logar}>Logar</button>
 
                 <div className="msg-login text-white text-center mt-2">
                     <span>
