445bbb798b4ab8c86d412fbddce8b18896b69b90
Iury Lemos <iurylemos10@gmail.com>
2019-12-27 17:17:22 -0300

    usando com o redux, reducer para salvar o store do usuario

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Provider from 'react-redux';
 
 //Páginas
 import Login from './view/login';
 import Cadastro from './view/cadastro';
 import Home from './view/home';
 
+//Reducer
+import store from '../src/store';
+
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={Home}></Route>
-      <Route exact path="/cadastro" component={Cadastro}></Route>
-      <Route exact path="/login" component={Login}></Route>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/cadastro" component={Cadastro}></Route>
+        <Route exact path="/login" component={Login}></Route>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/store/usuarioReducer.js
@@ -0,0 +1,19 @@
+//Aqui dentro vou definir dois estado, guardando o e-mail
+const INITIAL_STATE = {
+    usuarioEmail: '',
+    usuarioLogado: 0,
+};
+
+function usuarioReducer(state = INITIAL_STATE, action) {
+    switch(action.type) {
+        //Se o tipo for login, ele vai me retornar o estado atual  
+        case 'LOG_IN':
+            return { ...state, usuarioLogado: 1, usuarioEmail: action.usuarioEmail }
+        case 'LOG OUT':
+            return { ...state, usuarioLogado: 0, usuarioEmail: null }
+        default: 
+            return state;
+    }
+}
+
+export default usuarioReducer
