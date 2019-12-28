00d07f7833d493521bf6087511dc478fa0d2a641
Iury Lemos <iurylemos10@gmail.com>
2019-12-28 15:43:56 -0300

    utilizando useEffect, passando dados para o componente, e imprimindo nele

--- src/components/evento-card/index.js
+++ src/components/evento-card/index.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import './evento-card.css';
 import { Link } from 'react-router-dom';
+import firebase from '../../config/firebase';
 
-function EventoCard() {
+function EventoCard({ key, img, titulo, detalhes, visualizacoes }) {
+
+  const [urlImagem, setUrlImagem] = useState()
+
+  useEffect(() => {
+    firebase.storage().ref(`imagens/${img}`).getDownloadURL().then((url) => {
+      setUrlImagem(url)
+    })
+  })
+
+  //Recuperando as informações do input
+
   return (
     <div className="col-md-3 col-sm-12">
-      <img src="https://via.placeholder.com/100x50" className="card-img-top img-cartao" alt="Imagem do Evento"></img>
+      <img src={urlImagem} className="card-img-top img-cartao" alt="Imagem do Evento"></img>
 
       <div className="card-body">
-        <h5>Título do Evento</h5>
+        <h5>{titulo}</h5>
         <p className="card-text text-justify">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          {detalhes}
         </p>
 
         <div className="row rodape-card d-flex align-items-center">
@@ -19,7 +31,7 @@ function EventoCard() {
           </div>
 
           <div className="col-6 text-right">
-            <i className="fas fa-eye"></i><span>2020</span>
+            <i className="fas fa-eye"></i><span>{visualizacoes}</span>
           </div>
         </div>
 
--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+//USE EFFECT é um recurso que faz que quando a tela estiver carregada
+//Ele então executa algo..
 
 import './home.css';
 
@@ -6,12 +9,38 @@ import './home.css';
 //Componentes
 import NavBar from '../../components/navbar';
 import { useSelector } from 'react-redux';
-
+import firebase from '../../config/firebase'
 import EventoCard from '../../components/evento-card'
 
 function Home() {
-    return (
 
+
+    //Variavel para os eventos
+    //E assim estou dizendo que os eventos são um array
+    const [eventos, setEventos] = useState([]);
+    let listaEventos = [];
+
+    useEffect(() => {
+        firebase.firestore().collection('eventos').get().then(async (resultado) => {
+            await resultado.docs.forEach(doc => {
+                //Como dentro do documento eu possuo os id
+                //E dentro do id eu possuo os dados
+                //eu utilizo o operador de SPRAD ...
+                //E ele pega tudo que tem dentro desse id.
+                //
+
+                listaEventos.push({
+                    id: doc.id,
+                    ...doc.data()
+                })
+            })
+            setEventos(listaEventos);
+        })
+    })
+
+
+
+    return (
         // TAG FRAGMENT <>
         // É utilizada para devolver um único elemento
         //Ela não tem efeito de visual nenhum
@@ -21,7 +50,14 @@ function Home() {
             <h1>{useSelector(state => state.usuarioEmail)}</h1>
             <h1>Logado: {useSelector(state => state.usuarioLogado)}</h1>
 
-            <EventoCard></EventoCard>
+            <div className="row">
+                {
+                    //Aqui é um tipo de for com o map
+                    eventos.map(item => <EventoCard key={item.id} img={item.foto} titulo={item.titulo} detalhes={item.detalhes} visualizacoes={item.visualizacoes} ></EventoCard>)
+                }
+            </div>
+
+
         </>
 
     )
